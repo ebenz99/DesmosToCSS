4a7eff2eea40f8596fc9c8f4037f85cd93699285
Ethan Bensman <ebenz99@gmail.com>
2021-04-17 12:37:45 -0400

    seems to get correct info from desmos

---
+++ extension/contentScript.js
@@ -0,0 +1,122 @@
+// var nodes = document.evaluate("//div[@id='hello']", document, null, XPathResult.ANY_TYPE, null);
+// alert('testing');
+
+`
+<span style="width:.55em" class="dcg-mq-scaled dcg-mq-bracket-l dcg-mq-paren">
+    <svg preserveAspectRatio="none" viewBox="3 0 106 186">
+        <path d="M85 0 A61 101 0 0 0 85 186 L75 186 A75 101 0 0 1 75 0"></path>
+    </svg>
+</span>
+<span style="margin-left:.55em;margin-right:.55em" class="dcg-mq-bracket-middle dcg-mq-non-leaf" aria-hidden="true">
+    <var aria-hidden="true">t</var>
+</span>
+<span style="width:.55em" class="dcg-mq-scaled dcg-mq-bracket-r dcg-mq-paren">
+    <svg preserveAspectRatio="none" viewBox="3 0 106 186">
+        <path d="M24 0 A61 101 0 0 1 24 186 L34 186 A75 101 0 0 0 34 0"></path>
+    </svg>
+</span>`
+
+// alert(math.evaluate('sqrt(3^2 + 4^2)'))
+const NUM_EQUATIONS = 2;
+const OPEN_PAREN_SVG = '<path d="M85 0 A61 101 0 0 0 85 186 L75 186 A75 101 0 0 1 75 0"></path>';
+const CLOSED_PAREN_SVG = '<path d="M24 0 A61 101 0 0 1 24 186 L34 186 A75 101 0 0 0 34 0"></path>';
+
+const OPEN_PAREN_CLASS = 'dcg-mq-scaled dcg-mq-bracket-l dcg-mq-paren';
+const CLOSED_PAREN_CLASS = 'dcg-mq-scaled dcg-mq-bracket-r dcg-mq-paren';
+
+const IS_MIDDLE_BRACKET = (item) => {
+    if ((item.className == 'cg-mq-bracket-middle dcg-mq-non-leaf') || (item.className == 'dcg-mq-non-leaf dcg-mq-bracket-container')) {
+        return true;
+    }
+    return false;
+}
+
+// startIdx is idx of corresponding opening paren
+const getNextParenIdx = (elements, startIdx) => {
+    // console.log(elements)
+    numToPass = 0;
+    for(let i = startIdx+1; i < elements.length; i+=1){
+        if ((elements[i].className == OPEN_PAREN_CLASS) || (elements[i].innerHTML==OPEN_PAREN_SVG)) {
+            numToPass += 1;
+        }
+        if ((elements[i].className == CLOSED_PAREN_CLASS) || (elements[i].innerHTML==CLOSED_PAREN_SVG)) {
+            if (numToPass == 0){
+                return i;
+            }
+            numToPass -= 1;
+        }
+    }
+    console.log('UHOH');
+    return -1;
+}
+
+const interpretParens = (elementStr,flag) => {
+    // console.log(elementStr);
+    let currStr = "";  // output of everything between current parens
+    let wrapper = document.createElement( 'test' ); //create dummy element
+    wrapper.innerHTML = elementStr; // set wrapper innerHTML to parenthesized elements to convert to Nodes
+    let items = Array.from(wrapper.childNodes) // get HTML nodes of each top-level paren entry
+    let itemIdx = 0;
+    if (flag == true) {
+        console.log(items);
+    }
+    while (itemIdx < items.length){
+        if (IS_MIDDLE_BRACKET(items[itemIdx])) {
+            currStr += interpretParens(Array.from(items[itemIdx].childNodes).map((item) => {return item.innerHTML}).join(''), true);
+            itemIdx += 1;
+        }
+        else if ((items[itemIdx].className == OPEN_PAREN_CLASS) || (items[itemIdx].innerHTML==OPEN_PAREN_SVG) ){
+            currStr += '(';
+            endIdx = getNextParenIdx(items, itemIdx);
+            if (endIdx == -1) {
+                alert('ethanjs unmatched parens error')
+                return;
+            }
+            // console.log(currStr);
+            currStr += interpretParens(items.slice(itemIdx+1,endIdx).map((item) => {return item.innerHTML}).join(''), true);
+            currStr += ')';
+            itemIdx += endIdx + 1;
+        }
+        else {
+            // console.log(items[itemIdx].innerText)
+            if (items[itemIdx].textContent != "&nbsp;"){
+                currStr += items[itemIdx].textContent;
+            }
+            itemIdx += 1;
+        }
+    }
+    return currStr;
+    // el.getElementsByTagName( 'a' ); // Live NodeList of your anchor elements
+    
+}
+
+let elements = [];
+const xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "dcg-mq-root-block", " " ))]';
+var results = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+while (node = results.iterateNext()) {
+    elements.push(node)
+    // console.log(node);
+}
+
+elements = elements.slice(0,NUM_EQUATIONS);
+
+elements.map((equation) => {
+    let equationElements = Array.from(equation.children);
+    equationString = [equationElements.map((equationItem) => { 
+        if (equationItem.innerHTML.includes(OPEN_PAREN_CLASS)) {
+            return interpretParens(equationItem.innerHTML, false)
+        }
+        else if (equationItem.innerHTML.includes(CLOSED_PAREN_CLASS)) {
+            return '('
+        }
+        else {
+            return equationItem.innerHTML
+        }
+    })]
+    console.log(equationString)
+});
+
+
+// alert(elements[0].children)
+
+
--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,39 @@
+function getElementByXpath(doc, path) {
+    alert(doc)
+    return doc.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+
+// function test() {
+//     alert( getElementByXpath('//*[contains(concat( " ", @class, " " ), concat( " ", "dcg-mq-root-block", " " ))]') );
+// }
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // console.log("J'ai bien reçu un truc");
+    // console.log(request);
+    // console.log(sender);
+    alert('hi');
+}
+);
+
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
+    //*[contains(concat( " ", @class, " " ), concat( " ", "dcg-mq-root-block", " " ))]
     checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
+        chrome.tabs.getSelected(null, function(tab) {
+            d = document;
+            
+            // var f = d.createElement('form');
+            // f.action = 'http://gtmetrix.com/analyze.html?bm';
+            // f.method = 'post';
+            // var i = d.createElement('input');
+            // i.type = 'hidden';
+            // i.name = 'url';
+            // i.value = tab.url;
+            // f.appendChild(i);
+            // d.body.appendChild(f);
+            // f.submit();
+            alert( getElementByXpath(d,'//nobr') );
+        });
     }, false);
   }, false);
